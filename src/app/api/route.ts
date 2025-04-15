d63900cb358f6b718577e258572c5b715ae7b476
Bennor McCarthy <bennor.mccarthy@vercel.com>
2025-04-15 19:49:32 +1000

    Add custom rate limiting via SDK

---
+++ src/app/api/route.ts
@@ -0,0 +1,20 @@
+import { unstable_checkRateLimit as checkRateLimit } from '@vercel/firewall';
+import { NextResponse } from 'next/server';
+
+export async function POST(request: Request) {
+  const status = !(await request.clone().text()).match(/FAIL/i) ? 400 : 200;
+  const { rateLimited } = await checkRateLimit('update-object', {
+    request,
+    headers: { 'X-Status': status.toFixed() }
+  });
+  if (rateLimited) {
+    return NextResponse.json(
+      {
+        error: 'Rate limit exceeded'
+      },
+      { status: 429 }
+    );
+  }
+  // Otherwise, continue with other tasks
+  return NextResponse.json({ status: 'OK' });
+}
