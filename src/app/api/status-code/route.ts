fb86489fc37cda6c56a79c553984205190edf325
Bennor McCarthy <bennor.mccarthy@vercel.com>
2025-05-05 10:48:36 +1000

    Add additional endpoints

---
+++ src/app/api/status-code/route.ts
@@ -0,0 +1,29 @@
+import { unstable_checkRateLimit as checkRateLimit } from '@vercel/firewall';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(request: NextRequest) {
+  const status = (await request.clone().text()).match(/FAIL/i) ? 400 : 200;
+  const headers = {
+    ...Object.fromEntries(request.headers),
+    'x-status': status.toFixed()
+  };
+  const { rateLimited } = await checkRateLimit('status-check', {
+    request,
+    headers
+  });
+  if (rateLimited) {
+    return NextResponse.json(
+      {
+        error: 'Rate limit exceeded'
+      },
+      { status: 429 }
+    );
+  }
+  // Otherwise, continue with other tasks
+  return NextResponse.json(
+    {
+      status: status === 200 ? 'OK' : 'Bad Request'
+    },
+    { status }
+  );
+}
