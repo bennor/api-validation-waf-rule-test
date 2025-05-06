80988458d0803ea04f6b86d0508286d21a8b93af
Bennor McCarthy <bennor.mccarthy@vercel.com>
2025-05-06 12:24:43 +1000

    document `bad-request-only` example
    Added additional comments and context to `api/bad-request-only` route.

--- src/app/api/bad-request-only/route.ts
+++ src/app/api/bad-request-only/route.ts
@@ -2,10 +2,15 @@ import { unstable_checkRateLimit as checkRateLimit } from '@vercel/firewall';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(request: NextRequest) {
+  // Perform request validation. (This should replicate any validation performed
+  // in downstream APIs.)
   const status = (await request.clone().text()).match(/FAIL/i) ? 400 : 200;
+
   // Only check rate limiting if response status will be 400
-  // (Rate limit rule will apply to every request, but we're pre-filtering here.)
+  // (Rate limit rule in Vercel will apply to every request, but we're pre-filtering here.)
   if (status === 400) {
+    // The `rateLimitId` (`bad-request-only`) can be anything you like but must
+    // match the id applied in the rule in the Vercel Dashboard
     const { rateLimited } = await checkRateLimit('bad-request-only', {
       request
     });
@@ -18,11 +23,18 @@ export async function POST(request: NextRequest) {
       );
     }
   }
-  // Otherwise, continue with other tasks
+
+  // Otherwise, continue with other tasks (e.g. call a downstream API)
+  await callDownstreamApi(request);
+
   return NextResponse.json(
     {
       status: status === 200 ? 'OK' : 'Bad Request'
     },
     { status }
   );
+}
+
+async function callDownstreamApi(request: NextRequest) {
+  // Example only
 }
