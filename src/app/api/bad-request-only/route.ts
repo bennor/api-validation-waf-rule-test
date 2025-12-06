b9b84ca6999735b0cff71b20975685dd008d8921
Bennor McCarthy <bennor.mccarthy@vercel.com>
2025-12-07 07:48:58 +1100

    Fix lint error

--- src/app/api/bad-request-only/route.ts
+++ src/app/api/bad-request-only/route.ts
@@ -35,6 +35,7 @@ export async function POST(request: NextRequest) {
   );
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 async function callDownstreamApi(_request: NextRequest) {
   // Example only
 }
